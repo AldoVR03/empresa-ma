9a5b520b02c1f6ccf9a6b5770f684c5a524cb8bf
el_izquierdo <aldo.villarroel03@inacapmail.cl>
2023-11-15 23:30:50 -0300

    mejoras-login-registro

--- ma_app/models.py
+++ ma_app/models.py
@@ -1,85 +1,78 @@
 from django.db import models
 
-# Create your models here.
-
 class Administrador(models.Model):
     idAdministrador = models.AutoField(primary_key=True)
-    usuario = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
-    correo = models.CharField(max_length=30)
+    usuario = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    correo = models.EmailField()
+
+class Capacitacion(models.Model):
+    idCapacitacion = models.AutoField(primary_key=True)
+    nombreCapacitacion = models.CharField(max_length=100)
+    fechaDeEmicion = models.CharField(max_length=50)
 
 class Cliente(models.Model):
     idCliente = models.AutoField(primary_key=True)
-    nombreEmpresa = models.CharField(max_length=30)
-    rutEmpresa = models.IntegerField()
-    rut = models.IntegerField()
-    nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=30)
-    celular_Telefono = models.IntegerField()
-    password = models.CharField(max_length=30)
-    correo = models.CharField(max_length=30)
+    nombreEmpresa = models.CharField(max_length=50)
+    rutEmpresa = models.CharField(max_length=50)
+    celular_Telefono = models.IntegerField(null=True, blank=True)
+    correo = models.EmailField()
+    password = models.CharField(max_length=50)
+    razonSocial = models.CharField(max_length=50)
 
 class Cotizacion(models.Model):
-    idProyecto = models.IntegerField()
-    idCotizacion = models.IntegerField()
-    idCliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    nombreProyecto_Cantidad = models.CharField(max_length=30)
-    costo_Total = models.IntegerField()
-    descripcion = models.CharField(max_length=30)
-    fecha = models.CharField(max_length=30)
-
-    class Meta:
-        unique_together = ('idProyecto', 'idCotizacion')
-
-
-class GerenteAdmin(models.Model):
-    idGerenteAdmin = models.AutoField(primary_key=True)
-    usuario = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
-    correo = models.CharField(max_length=30)    
+    idServicio = models.ForeignKey('Servicio', on_delete=models.RESTRICT)
+    idCotizacion = models.AutoField(primary_key=True)
+    idCliente = models.ForeignKey(Cliente, on_delete=models.RESTRICT)
+    nombreProyecto = models.CharField(max_length=1)
+    costoTotal = models.IntegerField()
+    descripcion = models.CharField(max_length=1)
+    fecha = models.CharField(max_length=1)
 
 class Empleados(models.Model):
     idEmpleados = models.AutoField(primary_key=True)
-    idAdministrador = models.ForeignKey(Administrador, on_delete=models.CASCADE)
-    idGerenteAdmin = models.ForeignKey(GerenteAdmin, on_delete=models.CASCADE)
-    rol = models.CharField(max_length=30)
-    rut = models.IntegerField()
-    nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=30)
-    celular = models.IntegerField()
-    correo = models.CharField(max_length=30)
-    capacitacion = models.CharField(max_length=30)
+    rol = models.CharField(max_length=50)
+    rut = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    celular = models.IntegerField(null=True, blank=True)
+    correo = models.EmailField()
+    capacitacion = models.CharField(max_length=50)
 
+class GerenteAdmin(models.Model):
+    idGerenteAdmin = models.AutoField(primary_key=True)
+    usuario = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    correo = models.EmailField()
 
 class GestionNomina(models.Model):
+    idEmpleados = models.ForeignKey(Empleados, on_delete=models.RESTRICT)
     idGestionNomina = models.AutoField(primary_key=True)
-    idEmpleados = models.ForeignKey(Empleados, on_delete=models.CASCADE)
     sueldo = models.IntegerField()
     horasTrabajadas = models.IntegerField()
-    descansos = models.CharField(max_length=30)
-    tipoPago = models.IntegerField()
+    descansos = models.CharField(max_length=50)
+    tipoPago = models.CharField(max_length=50)
 
 class HistorialLaboral(models.Model):
+    idEmpleados = models.ForeignKey(Empleados, on_delete=models.RESTRICT)
     idHistoriaLab = models.AutoField(primary_key=True)
-    idEmpleados = models.ForeignKey(Empleados, on_delete=models.CASCADE)
-    permisos = models.CharField(max_length=30)
-    licenciasMedicas = models.CharField(max_length=30)
-    fallas = models.CharField(max_length=30)
-    comportamiento = models.CharField(max_length=30)
+    permisos = models.CharField(max_length=50)
+    licenciasMedicas = models.CharField(max_length=50)
+    fallas = models.CharField(max_length=50)
+    comportamiento = models.CharField(max_length=50)
     despido_Renuncia = models.BooleanField()
 
 class Inventario(models.Model):
-    idEmpleados = models.ForeignKey(Empleados, on_delete=models.CASCADE)
-    idArticulo = models.IntegerField()
-    nombreArticulo = models.CharField(max_length=30)
-    estadoArticulo = models.CharField(max_length=30)
-    cantidad = models.IntegerField()
+    idArticulo = models.AutoField(primary_key=True)
+    nombreArticulo = models.CharField(max_length=50)
+    estadoArticulo = models.CharField(max_length=50)
+    catindad = models.IntegerField()
 
-    class Meta:
-        unique_together = ('idEmpleados', 'idArticulo')
-
-
-class Proyecto(models.Model):
-    idProyecto = models.AutoField(primary_key=True)
-    nombreProyecto = models.CharField(max_length=30)
+class Servicio(models.Model):
+    idServicio = models.AutoField(primary_key=True)
+    nombreServicio = models.CharField(max_length=50)
     costo = models.IntegerField()
+
+class Association5(models.Model):
+    idCapacitacion = models.ForeignKey(Capacitacion, on_delete=models.RESTRICT)
+    idEmpleados = models.ForeignKey(Empleados, on_delete=models.RESTRICT)
--- ma_app/views.py
+++ ma_app/views.py
@@ -1,11 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ClienteForm
 
-# Create your views here.
-def login(request):
-    return render(request, 'login.html')
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .forms import ClienteForm 
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login as auth_login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import LoginForm
 
-def registro(request):
-    return render(request, 'registro.html')
+
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login
+from .forms import LoginForm
+
+from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login
+from django.shortcuts import render, redirect
+from .forms import LoginForm  # Asegúrate de importar el formulario adecuado desde tu aplicación
+
+
+@login_required  # Esto asegura que solo usuarios autenticados pueden acceder a esta vista
+def ver_cotizacion(request):
+    # Lógica para mostrar la página de ver_cotizacion.html
+    # Puedes agregar cualquier lógica adicional aquí según tus necesidades
+    return render(request, 'ver_cotizacion.html')
+
+
+
+def login_view(request):
+    if request.method == 'POST':
+        print("hola-test")
+        form = LoginForm(request, data=request.POST)
+        print("hola2")
+        user = authenticate(request, username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+        if user is not None:
+            print("hola3")
+            auth_login(request, user)
+            messages.success(request, 'Inicio de sesión exitoso.')
+
+            # Redirigir a la vista de ver_cotizacion en caso de inicio de sesión exitoso
+            return redirect('ver_cotizacion')
+        else:
+            messages.error(request, 'Error al iniciar sesión. Verifique sus credenciales.')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+    
+def registro_view(request):
+    if request.method == 'POST':
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Usuario registrado exitosamente.')
+    else:
+        form = ClienteForm()
+
+    return render(request, 'registro.html', {'form': form})
 
 def cotizacion(request):
     return render(request, 'cotizacion.html')
